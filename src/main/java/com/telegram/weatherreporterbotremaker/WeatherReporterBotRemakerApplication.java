5a599ab3dce5ad2d038de7adadc72fa410a03652
Korshunov Michael <kolin4325@gmail.com>
2023-07-26 21:19:40 +0300

    Создание проекта (добавление остальных файлов)

---
+++ src/main/java/com/telegram/weatherreporterbotremaker/WeatherReporterBotRemakerApplication.java
@@ -0,0 +1,13 @@
+package com.telegram.weatherreporterbotremaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherReporterBotRemakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherReporterBotRemakerApplication.class, args);
+    }
+
+}
